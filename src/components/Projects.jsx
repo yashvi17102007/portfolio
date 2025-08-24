35fa5a61610af46db143209658a3f1af99ecde57
yashvi patidar <patidaryashvi1@gmail.com>
2025-08-24 10:15:22 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,13 +15,38 @@ const projects = [
   },
   {
     title: "Music Player",
-    link: "https://musicplayer-r.netlify.app/",
+    link: "https://music-plyayer.netlify.app/",
     desc: "A stylish web-based music player built with React that allows users to play, pause, and switch between tracks with ease. It features a modern UI, album covers, and smooth audio controls for an engaging listening experience.",
   },
   {
-    title: "Book-My-show",
-    link: "https://bookmyyshow.netlify.app/",
-    desc: "A responsive movie ticket booking application inspired by BookMyShow, built with React. It allows users to explore movies, view details, and book tickets with an interactive and user-friendly interface.",
+    title: "Swiggy Clone (HTML + CSS)",
+    link: "https://swigyy.netlify.app/ ",
+    desc: "A static Swiggy homepage clone built using HTML and CSS. It features a clean and responsive design showcasing food delivery UI with modern styling and layout techniques.",
+  },
+  {
+    title: "PhonePe Clone (HTML + CSS)",
+    link: " https://phone-payy.netlify.app/",
+    desc: "A clone of PhonePe’s homepage developed using HTML and CSS, designed with a responsive layout and visually appealing interface to replicate the popular payment app’s look.",
+  },
+  {
+    title: "Clock (JavaScript)",
+    link: " https://cloc-k.netlify.app/ ",
+    desc: "An interactive analog clock built with JavaScript, HTML, and CSS. It dynamically updates in real-time, featuring smooth clock hand animations for hours, minutes, and seconds.",
+  },
+  {
+    title: "Digital Clock (JavaScript)",
+    link: " https://digital-colckk.netlify.app/",
+    desc: "A simple yet elegant digital clock project using JavaScript. It displays the current time in real-time with a stylish UI and 24/12-hour format toggle.",
+  },
+  {
+    title: "Hotel Website",
+    link: "https://hhotell.netlify.app/ ",
+    desc: "A responsive hotel booking website designed with HTML, CSS, and JavaScript. It includes sections for rooms, services, gallery, and contact details, with a user-friendly interface for showcasing hotel facilities.",
+  },
+  {
+    title: "Post Website",
+    link: "https://post-t.netlify.app/ ",
+    desc: "A blog-style post sharing website developed using HTML, CSS, and JavaScript. It allows users to browse posts, view details, and features a clean, modern layout for better readability.",
   },
 ];
 
